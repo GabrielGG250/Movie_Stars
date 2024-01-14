076654adef323c6af586ba75d638ffebbb4fb783
GabrielGG250 <gabe.academic@gmail.com>
2024-01-14 15:54:55 -0300

    MovieCard (sem estilo)

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import './App.css'
 function App() {
 
   return (
-    <div >
+    <div>
       <Navbar/>
       <main className='container'>
-        <h2>Movies Stars</h2>
         <Outlet />
       </main>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setTopMovies(data.results)
+    }
+
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+
+        console.log(topRatedUrl)
+
+        getTopRatedMovies(topRatedUrl)
+
+    }, [])
+
     return (
-        <div>Home</div>
+        <div>
+            <h2>Melhores filmes:</h2>
+            <div>
+                {topMovies.length === 0 && <p className="spinner-border"></p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
     )
 }
 
